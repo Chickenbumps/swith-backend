6e5a10758b495a22ab25596dea567663dc470a60
Chickenbumps <whgotjd46@gmail.com>
2021-11-21 02:13:07 +0900

    backend demo

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ import { SubscriptionServer } from "subscriptions-transport-ws";
 import { execute, subscribe } from "graphql";
 import { schema } from "./schema";
 import { Context } from "vm";
+import { ApolloServerPluginInlineTrace } from "apollo-server-core";
+import { env } from "process";
 
 const PORT = process.env.PORT;
 
@@ -20,6 +22,12 @@ const PORT = process.env.PORT;
   const server = new ApolloServer({
     schema,
     introspection: true,
+    apollo: {
+      key: env.APOLLO_KEY,
+      graphId: env.APOLLO_GRAPH_ID,
+      graphVariant: env.APOLLO_GRAPH_VARIANT,
+    },
+    plugins: [ApolloServerPluginInlineTrace()],
     context: async ({ req }): Promise<Context> => {
       return {
         loggedInUser: await getUser(req.headers.token),
